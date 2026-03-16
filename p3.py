93c6757ddf65317786032c1021f27193d435b88b
joselumar2019-dotcom <joselumar2019@gmail.com>
2026-03-16 12:02:37 +0100

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,32 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('Día 5: st.write')
+
+# Ejemplo 1: Texto y emojis
+st.write('Hola, *Mundo!* :sunglasses:')
+
+# Ejemplo 2: Números
+st.write(1234)
+
+# Ejemplo 3: Tablas (DataFrames)
+df = pd.DataFrame({
+     'primera columna': [1, 2, 3, 4],
+     'segunda columna': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Ejemplo 4: Mezclar argumentos
+st.write('Abajo hay una tabla:', df, 'Y arriba texto.')
+
+# Ejemplo 5: Gráficos complejos
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+
+grafico = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(grafico)
